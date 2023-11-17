32e586d6d7b4c5c1a877bfbd96620dd7c105ffb8
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-11-17 20:28:20 +0000

    Contact US form connected to backend. Still few things missing

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Session, Task
+from .models import db, User, Session, Task, Enquiries
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -14,6 +14,7 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Session, db.session))
     admin.add_view(ModelView(Task, db.session))
+    admin.add_view(ModelView(Enquiries,db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -98,7 +98,24 @@ class Test(db.Model):
             "id": self.id,
             "test_value": self.test_value,
         }
+class Enquiries(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    message = db.Column(db.String(450), nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    def __repr__(self):
+        return f'<Enquiries {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "message": self.message,
+           
+           
+        }
+  
 
 class StatisticTaskGenerator:
 
