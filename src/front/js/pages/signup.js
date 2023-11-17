a416dbf3ca920d57e9b9cdc1db3f7020f3e88398
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-11-17 19:18:17 +0000

    Changes retrieved from deleted branch and- Signup & ContactUS files upadted

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../styles/signup.css'; // Import the CSS file for styling
-import "../../styles/home.css";
 
-const Signup= () => {
+const Signup = () => {
   const [email, setEmail] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [password, setPassword] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [showPopup, setShowPopup] = useState(false);
 
   const handleSignup = (e) => {
     e.preventDefault();
+
+    // Check if any field is empty
+    if (!email || !firstName || !lastName || !password) {
+      setErrorMessage('Please fill in all the information.');
+      setSuccessMessage('');
+      return;
+    }
 
     // Perform signup logic with the form data
     // For example, you can send the data to a server or perform validation
@@ -19,12 +28,26 @@ const Signup= () => {
     setFirstName('');
     setLastName('');
     setPassword('');
+
+    setSuccessMessage('Account created successfully.');
+    setErrorMessage('');
+    setShowPopup(true);
   };
 
+  useEffect(() => {
+    if (showPopup) {
+      setTimeout(() => {
+        setShowPopup(false);
+      }, 2000); // Hide the popup after 2 seconds
+    }
+  }, [showPopup]);
+
   return (
-    <div style={{ height: '35vh', display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
-      <form className="bg-light" style={{ padding: '20px', borderRadius: '1px', width: '70%' }} onSubmit={handleSignup}>
+    <div style={{ height: '35vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <form className="bg-light" style={{ padding: '20px', borderRadius: '1px', width: '60%' }} onSubmit={handleSignup}>
         <h2>Signup Form</h2>
+        {showPopup && <div className="popup">{successMessage}</div>}
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
         <div className="form-group">
           <label>Email:</label>
           <input
@@ -64,7 +87,6 @@ const Signup= () => {
         
         <button type="submit" className="signup-button">Submit</button>
       
-        
         <a href="/login" class="login-button">Sign In</a>
        
       </form>
