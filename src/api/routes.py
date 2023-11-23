2bc29e5186f2ad304da55d378e678e7fdfbdcbc4
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-11-23 17:00:13 +0000

    Everything working fine-ContactUS form sending info to backend

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Session, Task, Test, StatisticTaskGenerator
+from api.models import db, User, Session, Task, Test, StatisticTaskGenerator, Enquiries
 from api.utils import generate_sitemap, APIException
 import datetime
 from sqlalchemy import delete, update
